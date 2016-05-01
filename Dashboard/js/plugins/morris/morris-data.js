483689558c9966fcdcee7b3d2cfeb2f98f0d7d73
Dillon Bostwick <dillon.bostwick@gmail.com>
2016-04-30 23:06:22 -0400

    changes

--- Dashboard/js/plugins/morris/morris-data.js
+++ Dashboard/js/plugins/morris/morris-data.js
@@ -88,97 +88,97 @@ $(function() {
         // the chart.
         data: [{
             d: '2012-10-01',
-            visits: 802
+            visits: 81
         }, {
             d: '2012-10-02',
-            visits: 783
+            visits: 92
         }, {
             d: '2012-10-03',
-            visits: 820
+            visits: 89
         }, {
             d: '2012-10-04',
-            visits: 839
+            visits: 98
         }, {
             d: '2012-10-05',
-            visits: 792
+            visits: 91
         }, {
             d: '2012-10-06',
-            visits: 859
+            visits: 100
         }, {
             d: '2012-10-07',
-            visits: 790
+            visits: 98
         }, {
             d: '2012-10-08',
-            visits: 1680
+            visits: 99
         }, {
             d: '2012-10-09',
-            visits: 1592
+            visits: 92
         }, {
             d: '2012-10-10',
-            visits: 1420
+            visits: 99
         }, {
             d: '2012-10-11',
-            visits: 882
+            visits: 98
         }, {
             d: '2012-10-12',
-            visits: 889
+            visits: 58
         }, {
             d: '2012-10-13',
-            visits: 819
+            visits: 99
         }, {
             d: '2012-10-14',
-            visits: 849
+            visits: 98
         }, {
             d: '2012-10-15',
-            visits: 870
+            visits: 85
         }, {
             d: '2012-10-16',
-            visits: 1063
+            visits: 100
         }, {
             d: '2012-10-17',
-            visits: 1192
+            visits: 100
         }, {
             d: '2012-10-18',
-            visits: 1224
+            visits: 99
         }, {
             d: '2012-10-19',
-            visits: 1329
+            visits: 98
         }, {
             d: '2012-10-20',
-            visits: 1329
+            visits: 42
         }, {
             d: '2012-10-21',
-            visits: 1239
+            visits: 99
         }, {
             d: '2012-10-22',
-            visits: 1190
+            visits: 95
         }, {
             d: '2012-10-23',
-            visits: 1312
+            visits: 82
         }, {
             d: '2012-10-24',
-            visits: 1293
+            visits: 89
         }, {
             d: '2012-10-25',
-            visits: 1283
+            visits: 82
         }, {
             d: '2012-10-26',
-            visits: 1248
+            visits: 92
         }, {
             d: '2012-10-27',
-            visits: 1323
+            visits: 98
         }, {
             d: '2012-10-28',
-            visits: 1390
+            visits: 100
         }, {
             d: '2012-10-29',
-            visits: 1420
+            visits: 99
         }, {
             d: '2012-10-30',
-            visits: 1529
+            visits: 100
         }, {
             d: '2012-10-31',
-            visits: 1892
+            visits: 92
         }, ],
         // The name of the data record attribute that contains x-visitss.
         xkey: 'd',
@@ -186,42 +186,14 @@ $(function() {
         ykeys: ['visits'],
         // Labels for the ykeys -- will be displayed when you hover over the
         // chart.
-        labels: ['Visits'],
+        labels: ['Score'],
         // Disables line smoothing
         smooth: false,
         resize: true
     });
 
     // Bar Chart
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            device: 'iPhone',
-            geekbench: 136
-        }, {
-            device: 'iPhone 3G',
-            geekbench: 137
-        }, {
-            device: 'iPhone 3GS',
-            geekbench: 275
-        }, {
-            device: 'iPhone 4',
-            geekbench: 380
-        }, {
-            device: 'iPhone 4S',
-            geekbench: 655
-        }, {
-            device: 'iPhone 5',
-            geekbench: 1571
-        }],
-        xkey: 'device',
-        ykeys: ['geekbench'],
-        labels: ['Geekbench'],
-        barRatio: 0.4,
-        xLabelAngle: 35,
-        hideHover: 'auto',
-        resize: true
-    });
+
 
 
 });
