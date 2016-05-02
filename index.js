32535e9971cf4f525041485a54a5a65a842d53cb
Vishnu Ravi <vishnu@vishnu.io>
2016-05-02 10:14:16 -0400

    Fix for undetected face

--- index.js
+++ index.js
@@ -24,24 +24,27 @@
     Webcam.attach('#my_camera');
     
     function take_snapshot() {
-    	Webcam.snap( function(data_uri) {
+    	Webcam.snap(function(data_uri) {
         // snap complete, image data is in 'data_uri'
         Webcam.upload(data_uri, 'https://autismtracker.vishnu.io/upload_img.php', function(code, text) {
-        obj = $.parseJSON(text);
-		anger = obj[0].scores.anger;
-        disgust = obj[0].scores.disgust;
-        contempt = obj[0].scores.contempt;
-        fear = obj[0].scores.fear;
-        happiness = obj[0].scores.happiness;
+	        if (text == "[]"){
+				$('#response').html('There was a problem detecting your face. Please try again.');
+			}else{
+		        obj = $.parseJSON(text);
+				anger = obj[0].scores.anger;
+		        disgust = obj[0].scores.disgust;
+		        contempt = obj[0].scores.contempt;
+		        fear = obj[0].scores.fear;
+		        happiness = obj[0].scores.happiness;
 
-		var emotions = [anger, disgust, contempt, fear, happiness],
-		maxEmotion = Math.max.apply(Math.max, emotions),
-		emotionNames = ["angry", "disgusted", "worried", "afraid", "happy"],
-		maxEmotionName = emotionNames[emotions.indexOf(maxEmotion)];
-		
-		$('#response').html("Your face looks " + maxEmotionName + ".");
-		window.expression = maxEmotionName;
-		
+				var emotions = [anger, disgust, contempt, fear, happiness],
+				maxEmotion = Math.max.apply(Math.max, emotions),
+				emotionNames = ["angry", "disgusted", "worried", "afraid", "happy"],
+				maxEmotionName = emotionNames[emotions.indexOf(maxEmotion)];
+				
+				$('#response').html("Your face looks " + maxEmotionName + ".");
+				window.expression = maxEmotionName;
+			}
         } );
 
     } );    
